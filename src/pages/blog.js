b0dc269ec39e2ea82d551adead1dd67b097296ad
Simar Singh <simarpaulsingh@gmail.com>
2018-10-02 21:59:49 -0400

    sort posts in reverse chronoligical order DESC frontmatter___date

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -24,7 +24,11 @@ const BlogPage = ({ data }) => (
 
 export const pageQuery = graphql`
   query BlogIndexQuery {
-    allMarkdownRemark {
+    allMarkdownRemark(
+     sort: {
+      fields: [frontmatter___date] 
+      order: DESC}
+    ) {
       edges {
         node {
           id
