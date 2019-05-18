54c01b5d9e2932ea0d63ce69b4cc2e3a7bfaf23a
Simar Singh <simarpaulsingh@gmail.com>
2019-05-18 03:01:48 -0400

    feat: updating about me with expereience and highlights

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,32 +12,116 @@ const AboutPage = () => (
       <a href='https://stackblitz.com/@simars' target='_blank'>StackBlitz</a>
     </h4>
 
-    <p>My Name is <em>Simar Paul Singh</em>. I have been professionally developing software since 2004.</p>
-
-    <h2>Programming Languages</h2>
-    <p>
-      Actively using <em>(Kotlin, Java), (ECMAScript 5/6/7, Typescript).</em> I also program in <em>(Python),
-      (Ruby)</em> and have interest in <em>(Go-Lang), (Er-Lang), (Haskell)</em>
+    <p>My Name is <em>Simar Paul Singh</em>. I have been professionally developing software since 2004. 
+    <br /> I am also a loved husband and father. Besides code, I like to play tennis and watch movies.
+    <br /> I am fond of reading and most recently even writing. Checkout my technical articles on <a href='https://medium.com/simars' target='_blank'>Medium</a>.
     </p>
 
-    <h2>Frameworks</h2>
     <p>
-      Actively using <em>(Spring on Java / Kotlin / Scala), (Koa.js / Express on Node), (Angular / React).
-    </em> I have keen interest in modern web front-end frameworks and technologies like web-components,
-      shadow-DOM, web-assembly etc.
+      <strong>Highlights</strong>
+      <span>
+        <br />• Presently developing Software AI / ML solution in Finance and Retail Domain
+        <br />• Full Stack Software Development &amp; Architecture experience (15+ years) in Finance, Retail &amp; Telecom
+        <br />• Polyglot programmer with strength in Java, JavaScript, ES 6/7, TypeScript, Kotlin, Ruby, Python, Shell
+        <br />• Strength in building Concurrent &amp; Distributed, Systems in Java, Scala Aka &amp; Node, Redis, Mongo
+        <br />• Experience in Enterprise technologies (J2EE, Spring, Hibernate, JPA, JBOSS, Ehcache, Infinispan)
+        <br />• Experience &amp; Interest in JS frameworks, Angular, Ionic, Polymer, React, Ext, Node, Grunt, Gulp, Webpack
+        <br />• Expert in building CI Systems (Docker, Git, Gradle, Jenkins, Train, BitBucket, Github, CircleCI etc)
+        <br />• Lead Development of Enterprise Grade, Transactional, Secure (DMZ) Web App in Finance domain
+        <br />• Developing Clustered Social Media Web App in (Node + Koa.js + Redis + MongoDB) &amp; (React + material-ui)
+        <br />• Strength &amp; Experience in Information and Web Security (SSL, TLS, HTTPS, Siteminder, OAuth, JWT)
+        <br />• Developing with Secure Sockets, Encryption, Tokenization, Digital Signatures &amp; Certs (JSSE, Bouncy Castle)
+        <br />• Developing Cloud Protection &amp; Residency Solution (Tokenization, Encryption) Java / Spring, JS / Angular
+        <br />• Developing Bean UI &amp; i8ln framework, Java Beans ->spring-> JSON -> angular JS -> UI
+        <br />• Interest in  Machine Learning / AI and built a image classifier in pithing using tensorflow
+        <br />• Developing Real-time Network, Billing, Rating, Charging, CRM solutions for Carriers in Telecom (2005 - 2013)
+        <br />• Proficient in REST, XML, SOAP/Web-Services, interface design &amp; development.
+        <br />• Proficient in SQL / Databases &amp; developing transaction oriented distributed systems.
+        <br />• Extensively worked on Solaris / Linux/ Unix platforms, JVM tuning &amp; scaling
+        <br />• Knowledge &amp; experience in Networking (TCP /IP) &amp; developing over TCP/IP Sockets
+        <br />• Experience on MS Server Platforms, C#/ASP.NET, IIS, Dynamics CRM/Entity Engine, C#/ASP.NET/MVC
+        <br />• Passionate about Knowledge Sharing. Actively participate &amp; conduct learning sessions
+    </span>
     </p>
 
-    <h2>Platforms</h2>
+
+    <h3>Work Experience</h3>
     <p>
-      Actively using <em>Linux</em> containerized manifestations <em>(Docker), (PCF), (BlueMix)</em>
-    </p>
-    <p>
-      I have been early adopter of <em>(Docker)</em> and have used it for production as well.
+      <strong>Element AI [2018 - Present]</strong>
+      <span>
+        <br />* Developing AL / ML based product for Financial Industry (Python, Flask, React, Material-UI)
+        <br />* Developoing an AI Vision based solution Retail Industry (React-Native Mobile App + Django REST / Channels)
+        <br />* Developing React UI Library and its build / packaging system (material-ui based Component, Themes, Styles)
+        <br />* Developing Grids &amp; Charts using React, dev-express, Victory-Charts and Material-UI
+        <br />* Fully Containerized, Cloud Native solution (Docker and Kubernetties)
+        <br />* CI using Git Flow, Docker, Kubernetties, CircleCi
+    </span>
     </p>
 
-    <h2>Machine Learning</h2>
-    <p>I have keen interest in Machine Learning. So far, I have learnt to build and use models in <em>(TensorFlow)</em>.
-      I would love to contribute professionally in Machine Learning. Most of my ML experience has been DIY.
+    <p>
+      <strong>RBC (ACE Labs) | Investor and Treasury Services [2018]</strong>
+      <span>
+        <br />* Developing Visualization, Analytics &amp; API Publishing Platform (Micro-services Architecture)
+        <br />* Developing system of SPAs (Angular 4/6) controlled by REST Micro Services (Spring Boot)
+        <br />* Developing UI catalogue of angular components, directives &amp; services (UI champions team)
+        <br />* Kotlin, Java 8, Spring Boot, Mongo, Elastic Stack, Kafka, PCF, IBM Bluemix
+        <br />* TypeScript, Angular 4/5/6 | JWT/JWKS + SSO, Git, Chunking Flow, Docker, Maven, Jenkins, Ensible 
+        <br />* Developing UI catalogue of angular components, directives &amp; services (UI champions team)
+    </span>
+    </p>
+
+    <p>
+      <strong>Alchemy Systems (bought by Intertec) [2018 - 2016]</strong>
+      <span>
+        <br />* Developing Multi-Tenant SaaS | Retail (Process, Assets, Location, Org., Workflow, Compliance)
+        <br />* Mobile First / Responsive Web Apps (Angular + Material) over REST Web Services (Spring MVC), Hibernate over MSSQL
+        <br />* Developed Routine-Task and scheduling which worked over 14 time zones and 40 diffrent locales.
+        <br />* Developed Calendar-Event &amp; Newsfeed system using bit-manipulation. (recurring and onetime)
+        <br />* Built Internationalized system for apps to efficiently work in 10+ languages. (jackson-serlizers + angular-translate)
+        <br />* Advanced Spring, Hibernate, , Jackson SerializersQuartz &amp; Infinispan (L2 Cache) over MSSQL
+        <br />* Developed Hierarchical Data Access &amp; Security for Multi-Tenancy, Ownership &amp; Geographical segregation.
+        <br /> AngularJS, Material, Apache &amp; Nginx | Git, Forking-Flow, Gulp | Docker
+    </span>
+    </p>
+
+    <p>
+      <strong>Morgan Stanley [2016 - 2014]</strong>
+      <span>
+        <br />* Leading Development &amp; Architecture of an Enterprize System where external clients manage &amp; digitally sign forex contracts
+        <br />* Developing Contract (Forex) Management &amp; Signing System with Enterprise Integration
+        <br />* Java EE 7, Guava Hibernate, Spring, MVC, EhCache, Apache CXF, Tomcat
+        <br />* JavaScript, ExtJs, AngularJS, SiteMinder
+        <br />* Digital Singatures with Bouncy Castle
+    </span>
+    </p>
+
+    <p>
+      <strong>PerspecSys Inc. (now Symantec) [2014 - 2012]</strong>
+      <span>
+        <br />* Developed (Prime) REST clients to fetch metadata and encrypted or tokenize structured data from salesforce instances.
+        <br />* Developed (in a team of 3) system of Policy Control that identify protected assets based on rules, applies relevant actions to get clear data internally.
+        <br />* Developed (Prime) a framework that renders and controls state of data objects from UI on web browser (Angular JS) to Data Service (Java E-Bean Store). 
+        <br />* Developed internationalization system integrated into this framework handling internationalization at client side (Browser), with dictionaries available from server.
+        <br />* Java, JSSE, Guava, Aka (Scala), Apache-HTTP-Components, Linux
+        <br />* Java, Spring MVC, REST, JavaScript / JQuery / Angular JS, HTML/CSS
+      </span>
+    </p>
+
+
+    <p>
+      <strong>Redknee Inc. (now Optiva Inc.) [2012 - 2005]</strong>
+      <span>
+        <br />* Architecture &amp; Implementation of Redknee OSS/BSS Systems API interconnecting with Microsoft Dynamics CRM
+        <br />* Developed  Event and Batch Processors for BSS (Billing Adjustment and Settlement)
+        <br />* Implemented Batch Processors, for bill cycle Recurring charges. Optimized for Throughput 
+        <br />* Implemented Real time rating and charging systems for telecom (Prepaid Airtime and Wallets)
+        <br />* Implemented a re-usable java library in Java providing dynamic web effects, like inline validations
+        <br />* Core member in Active migrations of Digicel Redknee Turnke Solution (major version upgrade)
+        <br />* Implemented fail-over / fail-back resilience library (CORBA + JacORB based)
+        <br />* Implemented Simple Text Messaging based applications (SMPP / SMS, USSD)
+        <br />* Java, Solaris CORBA, RMI, SOAP, Tomcat, Oracle
+        <br />* JavaScirpt, JQuery, HTML, XML / XSL
+      </span>
     </p>
 
   </div>
